1e6b0136e8257e2a5358660f42c33bd6b939f77c
sumant041 <sumantnidagundi525@gmail.com>
2016-11-09 12:09:40 +0530

    Tweaks

--- linkedListImplementation.ts
+++ linkedListImplementation.ts
@@ -9,6 +9,9 @@ let n3 = new LinkedListNode(3);
 ll.insert(0, n3);
 let n4 = new LinkedListNode(4);
 ll.insert(1, n4);
+let n5 = new LinkedListNode(5);
+ll.insert(2 ,n5);
 ll.print();
-ll.delete(1);
+ll.delete(3);
+
 ll.print();
